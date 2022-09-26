2ce22f3bc18b3078e57475ec00e8728602fd459a
DwrUisce <bellinir@tcd.ie>
2022-09-26 16:00:17 +0100

    v4

--- data.js
+++ data.js
@@ -24,36 +24,36 @@ var APP_DATA = {
       ],
       "faceSize": 1440,
       "initialViewParameters": {
-        "yaw": -0.11119810566366795,
-        "pitch": -0.002430919385073338,
-        "fov": 1.3937085184579643
+        "yaw": -0.13934526980397877,
+        "pitch": -0.09073111480407192,
+        "fov": 1.4743580089315886
       },
       "linkHotspots": [
         {
-          "yaw": -0.07028412103680459,
-          "pitch": 0.3048187966801805,
+          "yaw": -0.11175835960452574,
+          "pitch": 0.17038337535139902,
           "rotation": 0,
           "target": "2-cellar"
         },
         {
-          "yaw": -0.5800178558518247,
-          "pitch": -0.591656910786984,
+          "yaw": -0.4444099551650069,
+          "pitch": -0.4799634156708503,
           "rotation": 0,
           "target": "1-tearoom"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": -0.2651296139596582,
-          "pitch": -0.3583773143335982,
+          "yaw": -0.2542775091534093,
+          "pitch": -0.31970243113222097,
           "title": "Welcome",
-          "text": "<div>Welcome to a virtual tour of the heat recovery system at the Penrhyn Castle Tearooms. This system serves as a showcase of the potential to recover heat from the high temperature wastewater produced in commercial kitchens that usually just goes down the drain. Recovering this heat will save money by reducing fuel use and will also reduce the greenhouse gas emissions associated with heating water.&nbsp;&nbsp;</div><div><br></div><div>To start with the tour, please make your way into the tearooms by clicking on the ^ sign near the window above you and to your left.&nbsp;&nbsp;<br></div><div><br></div><div>This project is a collaboration between Bangor University, Trinity College Dublin, and the National Trust. It was made possible due to the support the ERDF Interreg Ireland-Wales Programme 2014-2020, through the Dŵr Uisce Project.</div>"
+          "text": "<div class=\"OutlineElement Ltr SCXW237767446 BCX2\" style=\"direction: ltr;\"><p class=\"Paragraph SCXW237767446 BCX2\" style=\"vertical-align: baseline; background-color: transparent;\" paraid=\"932057916\" paraeid=\"{47cbbb94-dd99-4db1-a501-f2593e094ded}{213}\"><font color=\"#000000\" face=\"Calibri, Calibri_EmbeddedFont, Calibri_MSFontService, sans-serif\"><span style=\"font-size: 14.6667px;\">Welcome to a virtual tour of the heat recovery system at the Penrhyn Castle Tearooms. This system serves as a showcase of the potential to recover heat from the high temperature wastewater produced in commercial kitchens that usually just goes down the drain. Recovering this heat will save money by reducing fuel use and will also reduce the greenhouse gas emissions associated with heating water.&nbsp;&nbsp;</span></font></p><p class=\"Paragraph SCXW237767446 BCX2\" style=\"vertical-align: baseline; background-color: transparent;\" paraid=\"932057916\" paraeid=\"{47cbbb94-dd99-4db1-a501-f2593e094ded}{213}\"><font color=\"#000000\" face=\"Calibri, Calibri_EmbeddedFont, Calibri_MSFontService, sans-serif\"><span style=\"font-size: 14.6667px;\"><br></span></font></p><p class=\"Paragraph SCXW237767446 BCX2\" style=\"vertical-align: baseline; background-color: transparent;\" paraid=\"932057916\" paraeid=\"{47cbbb94-dd99-4db1-a501-f2593e094ded}{213}\"><font color=\"#000000\" face=\"Calibri, Calibri_EmbeddedFont, Calibri_MSFontService, sans-serif\"><span style=\"font-size: 14.6667px;\">To start with the tour, please make your way into the tearooms by clicking on the ^ sign near the window above you and to your left.&nbsp;&nbsp;</span></font></p><p class=\"Paragraph SCXW237767446 BCX2\" style=\"vertical-align: baseline; background-color: transparent;\" paraid=\"932057916\" paraeid=\"{47cbbb94-dd99-4db1-a501-f2593e094ded}{213}\"><font color=\"#000000\" face=\"Calibri, Calibri_EmbeddedFont, Calibri_MSFontService, sans-serif\"><span style=\"font-size: 14.6667px;\"><br></span></font></p><p class=\"Paragraph SCXW237767446 BCX2\" style=\"vertical-align: baseline; background-color: transparent;\" paraid=\"932057916\" paraeid=\"{47cbbb94-dd99-4db1-a501-f2593e094ded}{213}\"><font color=\"#000000\" face=\"Calibri, Calibri_EmbeddedFont, Calibri_MSFontService, sans-serif\"><span style=\"font-size: 14.6667px;\">This project is a collaboration between Bangor University, Trinity College Dublin, and the National Trust. It was made possible due to the support the ERDF Interreg Ireland-Wales Programme 2014-2020, through the Dŵr Uisce Project.</span></font></p></div>"
         },
         {
-          "yaw": 0.6493467962149406,
-          "pitch": 0.15691198254155125,
-          "title": "Coalyard",
-          "text": "Background on the Coalyard site and how it was used."
+          "yaw": 0.06741044166788157,
+          "pitch": -0.05568808251317847,
+          "title": "The Coalyard and Penrhyn Castle",
+          "text": "<span style=\"color: windowtext; font-size: 11pt; line-height: 19.425px; font-family: Calibri, &quot;Calibri_EmbeddedFont&quot;, &quot;Calibri_MSFontService&quot;, sans-serif;\" data-contrast=\"auto\" xml:lang=\"EN-US\" class=\"TextRun SCXW118143311 BCX2\" lang=\"EN-US\"><span class=\"NormalTextRun SCXW118143311 BCX2\"></span><span class=\"NormalTextRun SCXW118143311 BCX2\">Penrhyn Castle was built in the Neo-Norman style between 1822 and 1837 by Thomas Hopper for George Hay Dawkins-Pennant. This large 204 room castle was built using profits from the Penrhyn Quarry, which was in turn expanded on by profits from sugar plantations in Jamaica worked on predominantly by enslaved people. Colonel Edward Gordon Douglas-Pennant inherited the castle and expanded it significantly following his rise in status to the 1st Lord Penrhyn. This Coal Yard was constructed as a part of this expansion in 1868 and it predominantly held wood and coal for use in the servant’s quarters. The Castle was given to the National Trust to be managed in 1951 after the death of the 4th Lord Penrhyn, Hugh Napier, in 1949.</span></span>"
         }
       ]
     },
@@ -81,24 +81,24 @@ var APP_DATA = {
       ],
       "faceSize": 1440,
       "initialViewParameters": {
-        "yaw": -0.0004109392066720119,
-        "pitch": 0.33490952906851135,
+        "yaw": 0.07882132105781281,
+        "pitch": -0.08694931847545107,
         "fov": 1.4743580089315886
       },
       "linkHotspots": [
         {
-          "yaw": 0.7980979318972796,
-          "pitch": -0.05899052597057519,
+          "yaw": 0.8304408714556857,
+          "pitch": -0.07175618801726813,
           "rotation": 0,
           "target": "0-coalyard"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": -0.1711682469639655,
-          "pitch": -0.01222551329749777,
-          "title": "Drain Water from Kitchens",
-          "text": "<div>After you enjoy a nice meal or snack at the Penrhyn Castle Tearooms, Tearoom Staff use hot water at a temperature of around 40-50 degrees celsius to clean dirty dishes and cooking utensils. This hot drain water then flows into a drain, through a sewer and to a septic tank within the Penrhyn Castle site, slowly cooling along the way. Our system aims to recover some of this waste heat and reduce water heating costs for the Tearoom kitchen.&nbsp;&nbsp;</div><div><br></div><div>Please continue the tour by clicking on the ^ sign near the window to take you back to the Coalyard and then click on the second ^ sign in front of you to take you to the Cellar area.</div>"
+          "yaw": -0.1735313322734271,
+          "pitch": 0.023123276421358696,
+          "title": "Drain Water From Kitchens",
+          "text": "<div class=\"OutlineElement Ltr SCXW52282164 BCX2\" style=\"direction: ltr;\"><p class=\"Paragraph SCXW52282164 BCX2\" style=\"vertical-align: baseline; background-color: transparent;\" paraid=\"1230981791\" paraeid=\"{c34f5c8e-2611-41ff-8810-0c38d0731dcb}{40}\"><font color=\"#000000\">After you enjoy a nice meal or snack at the Penrhyn Castle Tearooms, Tearoom Staff use hot water at a temperature of around 40-50 degrees celsius to clean dirty dishes and cooking utensils. This hot drain water then flows into a drain, through a sewer and to a septic tank within the Penrhyn Castle site, slowly cooling along the way. Our system aims to recover some of this waste heat and reduce water heating costs for the Tearoom kitchen.&nbsp;&nbsp;</font></p><p class=\"Paragraph SCXW52282164 BCX2\" style=\"vertical-align: baseline; background-color: transparent;\" paraid=\"1230981791\" paraeid=\"{c34f5c8e-2611-41ff-8810-0c38d0731dcb}{40}\"><font color=\"#000000\"><br></font></p><p class=\"Paragraph SCXW52282164 BCX2\" style=\"vertical-align: baseline; background-color: transparent;\" paraid=\"1230981791\" paraeid=\"{c34f5c8e-2611-41ff-8810-0c38d0731dcb}{40}\"><font color=\"#000000\">Please continue the tour by clicking on the ^ sign near the window to take you back to the Coalyard and then click on the second ^ sign in front of you to take you to the cellar area.</font></p></div>"
         }
       ]
     },
@@ -126,42 +126,42 @@ var APP_DATA = {
       ],
       "faceSize": 1440,
       "initialViewParameters": {
-        "yaw": 0.27946168074761424,
-        "pitch": -0.18903516160999345,
+        "yaw": 0.3814573157405263,
+        "pitch": -0.14557033876020142,
         "fov": 1.4743580089315886
       },
       "linkHotspots": [
         {
-          "yaw": -2.0329777759161516,
-          "pitch": -0.12801952558406882,
-          "rotation": 0,
-          "target": "3-control-room"
-        },
-        {
-          "yaw": -0.8041458368612879,
-          "pitch": -0.009579050268996525,
+          "yaw": -0.7251406708234889,
+          "pitch": -0.1321225671195556,
           "rotation": 0,
           "target": "0-coalyard"
+        },
+        {
+          "yaw": -2.2194281898574015,
+          "pitch": -0.19718454425369103,
+          "rotation": 0,
+          "target": "3-control-room"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": -0.046318194286737224,
-          "pitch": -0.23244003026117355,
-          "title": "Heat Exchanger 1",
-          "text": "<font face=\"Calibri, Calibri_EmbeddedFont, Calibri_MSFontService, sans-serif\"><span style=\"font-size: 14.6667px;\">The kitchen drain that was here has been replaced with an insulated copper heat exchanger. This heats up clean cold water that is pumped&nbsp; upwards through an outer copper pipe within which is another central large copper pipe, through which the drain water flows down. Only heat is transferred through the copper and heats up the cold water which is circulated from and back into the storage tank in the control room.</span></font><br><div><span data-contrast=\"auto\" style=\"font-size: 11pt; line-height: 19.425px; font-family: Calibri, &quot;Calibri_EmbeddedFont&quot;, &quot;Calibri_MSFontService&quot;, sans-serif;\" xml:lang=\"EN-US\" class=\"TextRun SCXW1634664 BCX2\" lang=\"EN-US\"><span class=\"NormalTextRun SCXW1634664 BCX2\"><br></span></span></div><div><span data-contrast=\"auto\" style=\"font-size: 11pt; line-height: 19.425px; font-family: Calibri, &quot;Calibri_EmbeddedFont&quot;, &quot;Calibri_MSFontService&quot;, sans-serif;\" xml:lang=\"EN-US\" class=\"TextRun SCXW1634664 BCX2\" lang=\"EN-US\"><span class=\"NormalTextRun SCXW1634664 BCX2\">Move around to continue the tour.</span></span></div>"
+          "yaw": 0.007374569296134226,
+          "pitch": -0.18113870241464625,
+          "title": "<span data-contrast=\"auto\" style=\"font-size: 11pt; line-height: 19.425px; font-family: Calibri, &quot;Calibri_EmbeddedFont&quot;, &quot;Calibri_MSFontService&quot;, sans-serif;\" xml:lang=\"EN-US\" class=\"TextRun SCXW11718509 BCX2\" lang=\"EN-US\"><span class=\"NormalTextRun SCXW11718509 BCX2\">Heat Exchanger</span><span class=\"NormalTextRun SCXW11718509 BCX2\"> 1</span></span>",
+          "text": "<div><font face=\"Calibri, Calibri_EmbeddedFont, Calibri_MSFontService, sans-serif\"><span style=\"font-size: 14.6667px;\">The kitchen drain that was here has been replaced with an insulated copper heat exchanger. This heats up clean cold water that is pumped upwards through an outer copper pipe within which is another central large copper pipe, through which the drain water flows down. Only heat is transferred through the copper and heats up the cold water which is circulated from and back into the storage tank in the control room.&nbsp;&nbsp;</span></font></div><div><font face=\"Calibri, Calibri_EmbeddedFont, Calibri_MSFontService, sans-serif\"><span style=\"font-size: 14.6667px;\"><br></span></font></div><div><font face=\"Calibri, Calibri_EmbeddedFont, Calibri_MSFontService, sans-serif\"><span style=\"font-size: 14.6667px;\">Move around to continue the tour.&nbsp;&nbsp;</span></font></div>"
         },
         {
-          "yaw": -2.6894818692883344,
-          "pitch": -0.42722225771147926,
-          "title": "Monitoring System",
-          "text": "No demonstration is complete without an understanding of how well it works! This remote monitoring system collects data about the performance of the system for future research and improvements.<div><br></div><div>Click on the ^ button on the door to continue the tour.</div>"
-        },
-        {
-          "yaw": -0.20456292529506115,
-          "pitch": -0.49306100052504753,
+          "yaw": -0.20651259438216485,
+          "pitch": -0.4746157621997824,
           "title": "Heat Exchanger 2",
-          "text": "A second heat exchanger which serves the same purpose as Heat Exchanger 1."
+          "text": "A second heat exchanger that serves the same purpose as Heat Exchanger 1."
+        },
+        {
+          "yaw": -2.6516307336607454,
+          "pitch": -0.4344392263837875,
+          "title": "Monitoring Station",
+          "text": "<div>No demonstration is complete without an understanding of how well it works! This remote monitoring system collects data about the performance of the system for future research and improvements.&nbsp;</div><div><br></div><div>Click on the ^ button on the door to continue the tour.&nbsp;</div>"
         }
       ]
     },
@@ -189,75 +189,68 @@ var APP_DATA = {
       ],
       "faceSize": 1440,
       "initialViewParameters": {
-        "yaw": -0.5292288699595726,
-        "pitch": -0.13609458543984942,
+        "yaw": -0.06329730542481649,
+        "pitch": -0.14178770656025463,
         "fov": 1.4743580089315886
       },
-      "linkHotspots": [
-        {
-          "yaw": 2.6094146613961993,
-          "pitch": -0.13810217751811216,
-          "rotation": 0,
-          "target": "2-cellar"
-        }
-      ],
+      "linkHotspots": [],
       "infoHotspots": [
         {
-          "yaw": -0.5889466142563169,
-          "pitch": -0.5281669998282226,
-          "title": "Storage Tank",
-          "text": "<span data-contrast=\"auto\" style=\"font-size: 11pt; line-height: 19.425px; font-family: Calibri, &quot;Calibri_EmbeddedFont&quot;, &quot;Calibri_MSFontService&quot;, sans-serif;\" xml:lang=\"EN-US\" class=\"TextRun SCXW122330529 BCX2\" lang=\"EN-US\"><span class=\"NormalTextRun SCXW122330529 BCX2\">W</span><span class=\"NormalTextRun SCXW122330529 BCX2\">ater is pumped out and to the heat exchanger from this tank. It also supplies the Mixing unit with water heated through heat recovery from the drains.&nbsp;</span></span>"
-        },
-        {
-          "yaw": -0.3235600346529477,
-          "pitch": 0.09228729771168354,
+          "yaw": -0.31979436938295436,
+          "pitch": 0.11639836479255017,
           "title": "Temperature Probe",
           "text": "A sensor that measures hot water temperature which is recorded in the monitoring station.&nbsp;"
         },
         {
-          "yaw": -1.0854073534606563,
-          "pitch": -0.22319292675919478,
+          "yaw": -0.5416262838210422,
+          "pitch": -0.5097984374939291,
+          "title": "Storage Tank",
+          "text": "Water is pumped out and to the heat exchanger from this tank. It also supplies the Mixing unit with water heated through heat recovery from the heat exchangers.&nbsp;"
+        },
+        {
+          "yaw": -1.00480149753486,
+          "pitch": -0.2036564269115786,
           "title": "Supply Pump",
-          "text": "<span data-contrast=\"auto\" style=\"font-size: 11pt; line-height: 19.425px; font-family: Calibri, &quot;Calibri_EmbeddedFont&quot;, &quot;Calibri_MSFontService&quot;, sans-serif;\" xml:lang=\"EN-US\" class=\"TextRun SCXW240232794 BCX2\" lang=\"EN-US\">This pump supplies heated water to the Mixing Unit.</span>"
+          "text": "This pump supplies heated water to the Mixing Unit."
         },
         {
-          "yaw": 0.028822201928010216,
-          "pitch": -0.10770993139045437,
-          "title": "Recirculation Pump",
-          "text": "This pump transfers cold water into and warm water out of the heat exchanger.&nbsp;"
-        },
-        {
-          "yaw": 0.2778040736060525,
-          "pitch": -0.09587300158065659,
-          "title": "Piping",
-          "text": "These pipes lead cold water to and hot water from from the heat exchanger in the cellar, see if you can follow them all the way to the heat exchanger!"
-        },
-        {
-          "yaw": 0.29572691387389227,
-          "pitch": 0.13420797696920417,
+          "yaw": 0.36230928367123383,
+          "pitch": -0.07066519223998213,
           "title": "Piping",
           "text": "These pipes lead cold water to and hot water from the heat exchanger in the cellar, see if you can follow them all the way to the heat exchanger!"
         },
         {
-          "yaw": -1.9895550053924325,
-          "pitch": -0.5794979882160494,
+          "yaw": 0.35249148999105095,
+          "pitch": 0.15916379604048636,
+          "title": "Piping",
+          "text": "These pipes lead cold water to and hot water from the heat exchanger in the cellar, see if you can follow them all the way to the heat exchanger!"
+        },
+        {
+          "yaw": 0.04627296973455941,
+          "pitch": -0.1156396129568602,
+          "title": "Recirculation Pump",
+          "text": "This pump transfers cold water into and warm water out of the heat exchanger."
+        },
+        {
+          "yaw": -1.927402567354612,
+          "pitch": -0.60075007483983,
           "title": "Mixing Unit",
           "text": "This unit mixes the water heated by the heat exchanger from the storage tank with water heated by the Castle’s boilers to supply hot water at a suitable temperature to the Tearoom kitchens.&nbsp;"
         },
         {
-          "yaw": 3.0604502128664564,
-          "pitch": -0.13296927864668007,
-          "title": "Thank you",
-          "text": "Once you finish looking around this room, this concludes our tour. Thank you for your interest in our project!&nbsp;"
+          "yaw": -3.0616786412521115,
+          "pitch": -0.294145650272684,
+          "title": "Thank You!",
+          "text": "Once you finish looking around this room, this concludes our tour. Thank you for your interest in our project!"
         }
       ]
     }
   ],
-  "name": "Penrhyn Castle Tour English",
+  "name": "Penrhyn Castle English",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
-    "fullscreenButton": false,
+    "fullscreenButton": true,
     "viewControlButtons": false
   }
 };
